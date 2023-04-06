a46c35806ff32851b6dfc491083f3e4573d9a0a9
Paul Easter <paul.john.easter@gmail.com>
2023-04-06 17:44:04 +1000

    Reverted back to tutorial code

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -14,30 +14,8 @@ public class HelloWorldController : Controller
     // 
     // GET: /HelloWorld/Welcome/ 
     // Requires using System.Text.Encodings.Web;
-    public string Welcome(string? name, int? numTimes)
+    public string Welcome(string name, int ID = 1)
     {
-        string? name_string = null;
-        string? times_string = null;
-        bool empty_response = true;
-
-        if (null != name)
-        {
-            name_string = $"Hello {name}";
-            empty_response = false;
-        }
-        if (null != numTimes)
-        {
-            times_string = $"NumTimes is: {numTimes}";
-            empty_response = false;
-        }
-
-        if (empty_response)
-        {
-            return HtmlEncoder.Default.Encode($"No options sent");
-        }
-        else
-        {
-            return HtmlEncoder.Default.Encode(name_string + " " + times_string);
-        }
+        return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
     }
 }
